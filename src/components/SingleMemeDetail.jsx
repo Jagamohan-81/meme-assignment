5072abcba7c9e61d402cdba723df6b9cf0358ed7
Jagamohan Panda <pandajagamohan12@gmail.com>
2023-02-13 14:54:13 +0530

    modal-update

---
+++ src/components/SingleMemeDetail.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function SingleMemeDetail({ elm }) {
+  //   return (
+  //     <div
+  //       className="card m-1 container"
+  //       style={{ width: "19%" }}
+  //       key={elm.id}
+  //       onClick={(e) => {
+  //         showModal(e, elm);
+  //       }}
+  //     >
+  //       {wishlistData.indexOf(elm) < 0 ? (
+  //         <BsSuitHeart
+  //           onClick={(e) => {
+  //             handleAdd(e, elm);
+  //           }}
+  //           className="icon fa-lg"
+  //         >
+  //           Add to Whishlist
+  //         </BsSuitHeart>
+  //       ) : (
+  //         <BsSuitHeartFill
+  //           style={{ color: "red" }}
+  //           onClick={(e) => {
+  //             handleRemove(e, elm);
+  //           }}
+  //           className="icon fa-lg"
+  //         />
+  //       )}
+  //       <img className="card-img-top" src={elm.url} alt="Card image cap" />
+  //       <div className="card-body">
+  //         <h5 className="card-title">{elm.name}</h5>
+  //       </div>
+  //     </div>
+  //   );
+}
+
+export default SingleMemeDetail;
